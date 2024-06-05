32502dbd46b6d94236d8f856d81919e182bb0f32
Bart <bartlomiej.sokol123@gmail.com>
2024-06-05 22:49:21 +0100

    fixed filter

--- main.py
+++ main.py
@@ -13,22 +13,24 @@ csrf = CSRFProtect(app)
 
 
 class FilterForm(FlaskForm):
-    min_rooms = SelectField('', choices=[(0, "Min pokoje"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5+")])
-    max_rooms = SelectField('', choices=[(1001, "Max pokoje"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (100, "5+")])
+    min_rooms = SelectField('', choices=[(0, "Min pokoje"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5+")],
+                            validate_choice=False)
+    max_rooms = SelectField('', choices=[(1001, "Max pokoje"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (100, "5+")],
+                            validate_choice=False)
     min_area = SelectField('', choices=[(0, "Min m2"), (10, "10m2"), (20, "20m2"), (30, "30m2"), (40, "40m2"),
                                         (50, "50m2"), (60, "60m2"), (70, "70m2"), (80, "80m2"), (90, "90m2"),
                                         (100, "100m2"), (110, "110m2"), (120, "120m2"), (130, "130m2"), (140, "140m2"),
-                                        (150, "150m2+")])
+                                        (150, "150m2+")], validate_choice=False)
     max_area = SelectField('', choices=[(1001, "Max m2"), (10, "10m2"), (20, "20m2"), (30, "30m2"), (40, "40m2"),
                                         (50, "50m2"), (60, "60m2"), (70, "70m2"), (80, "80m2"), (90, "90m2"),
                                         (100, "100m2"), (110, "110m2"), (120, "120m2"), (130, "130m2"), (140, "140m2"),
-                                        (1000, "150m2+")])
+                                        (1000, "150m2+")], validate_choice=False)
     min_floor = SelectField('', choices=[(0, "Min piętro"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5"), (6, "6"),
                                          (7, "7"), (8, "8"), (9, "9"), (10, "10"), (11, "11"), (12, "12"), (13, "13"),
-                                         (14, "14"), (15, "15+")])
+                                         (14, "14"), (15, "15+")], validate_choice=False)
     max_floor = SelectField('', choices=[(101, "Max piętro"), (1, "1"), (2, "2"), (3, "3"), (4, "4"), (5, "5"), (6, "6"),
                                          (7, "7"), (8, "8"), (9, "9"), (10, "10"), (11, "11"), (12, "12"), (13, "13"),
-                                         (14, "14"), (100, "15+")])
+                                         (14, "14"), (100, "15+")], validate_choice=False)
     rent_sell = SelectField('', choices=[(0, "Na sprzedaż"), (1, "Na wynajem")])
     submit = SubmitField('Filter')
 
@@ -60,8 +62,7 @@ def basic_select2():
 
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
-
-def filter_select(city, min_rooms, max_rooms, min_area, max_area, min_floor, max_floor, rent_sell):
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 
     POSTGRES_DATABASE_HOST_ADDRESS = os.environ.get('POSTGRES_DATABASE_HOST_ADDRESS')
     POSTGRES_DATABASE_NAME = os.environ.get('POSTGRES_DATABASE_NAME')
@@ -72,6 +73,22 @@ def filter_select(city, min_rooms, max_rooms, min_area, max_area, min_floor, max
     db_info = "host='%s' dbname='%s' user='%s' password='%s' sslmode='require'  port='%s'" % (
     POSTGRES_DATABASE_HOST_ADDRESS, POSTGRES_DATABASE_NAME, POSTGRES_USERNAME, POSTGRES_PASSWORD,
     POSTGRES_CONNECTION_PORT)
+    con = psycopg2.connect(db_info)
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+def filter_select(city, min_rooms, max_rooms, min_area, max_area, min_floor, max_floor, rent_sell):
+
+    POSTGRES_DATABASE_HOST_ADDRESS = os.environ.get('POSTGRES_DATABASE_HOST_ADDRESS')
+    POSTGRES_DATABASE_NAME = os.environ.get('POSTGRES_DATABASE_NAME')
+    POSTGRES_USERNAME = os.environ.get('POSTGRES_USERNAME')
+    POSTGRES_PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    POSTGRES_CONNECTION_PORT = "5432"
+
+    db_info = "host='%s' dbname='%s' user='%s' password='%s' sslmode='require'  port='%s'" % (
+        POSTGRES_DATABASE_HOST_ADDRESS, POSTGRES_DATABASE_NAME, POSTGRES_USERNAME, POSTGRES_PASSWORD,
+        POSTGRES_CONNECTION_PORT)
     con = psycopg2.connect(db_info)
     cur = con.cursor()
 
@@ -120,15 +137,13 @@ def validation_if_min_greater_than_max(form):
     if form.min_area.data is None or form.max_area.data is None:
         pass
     elif int(form.min_area.data) > int(form.max_area.data):
-        form.area_rooms.data = form.max_area.choices[0][0]
+        form.max_area.data = form.max_area.choices[0][0]
     if form.min_floor.data is None or form.max_floor.data is None:
         pass
     elif int(form.min_floor.data) > int(form.max_floor.data):
         form.max_floor.data = form.max_floor.choices[0][0]
 
     return [form.max_rooms.data, form.max_area.data, form.max_floor.data]
-
-
 
 
 @app.route('/', methods=['GET', 'POST'])
